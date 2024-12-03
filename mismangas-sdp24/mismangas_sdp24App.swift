25b38a0e91945c262845352ebf5d546dcfb18396
José Mª Pecharromán <josemariapecharroman@gmail.es>
2024-12-03 21:51:49 +0100

    Initial Commit

---
+++ mismangas-sdp24/mismangas_sdp24App.swift
@@ -0,0 +1,17 @@
+//
+//  mismangas_sdp24App.swift
+//  mismangas-sdp24
+//
+//  Created by José Mª Pecharromán on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct mismangas_sdp24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
