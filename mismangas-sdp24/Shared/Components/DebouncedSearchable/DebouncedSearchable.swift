dcaf13e4d1b23d6a80e16e6f0a4075b6097c9164
José Mª Pecharromán <josemariapecharroman@icloud.com>
2024-12-28 13:56:03 +0100

    Mejoras en DebouncedSearchable

--- mismangas-sdp24/Shared/Components/DebouncedSearchable/DebouncedSearchable.swift
+++ mismangas-sdp24/Shared/Components/DebouncedSearchable/DebouncedSearchable.swift
@@ -9,15 +9,16 @@ import SwiftUI
 
 struct DebouncedSearchable: ViewModifier {
     @Binding var text: String
+    @Binding var isPresented: Bool
     let delay: Duration
     let action: @MainActor (String) async -> Void
     
-    @State private var isPresented: Bool = false
+    @State private var isPresentedInternal: Bool = false
     @State private var searchTask: Task<Void, Never>?
     
     func body(content: Content) -> some View {
         content
-            .searchable(text: $text, isPresented: $isPresented)
+            .searchable(text: $text, isPresented: $isPresentedInternal)
             .onChange(of: text) {
                 searchTask?.cancel()
                 searchTask = Task {
@@ -29,11 +30,19 @@ struct DebouncedSearchable: ViewModifier {
                     await action(text)
                 }
             }
+            .onChange(of: isPresentedInternal) {
+                guard isPresented != isPresentedInternal else { return }
+                isPresented = isPresentedInternal
+            }
+            .onChange(of: isPresented) {
+                guard isPresented != isPresentedInternal else { return }
+                isPresentedInternal = isPresented
+            }
     }
 }
 
 extension View {
-    func debouncedSearchable(text: Binding<String>, delay: Duration = .seconds(1.0), action: @escaping @MainActor (String) async -> Void) -> some View {
-        modifier(DebouncedSearchable(text: text, delay: delay, action: action))
+    func debouncedSearchable(text: Binding<String>, isPresented: Binding<Bool> = .constant(true), delay: Duration = .seconds(1.0), action: @escaping @MainActor (String) async -> Void) -> some View {
+        modifier(DebouncedSearchable(text: text, isPresented: isPresented, delay: delay, action: action))
     }
 }
